ec34056942b422fae4f6881d07ad90d03ef7a7e5
unknown <sso9594@naver.com>
2025-04-07 15:27:57 +0900

    Init ssafy5 PRJ

---
+++ src/main/java/com/ssafy/ssafy_5_Trip/Ssafy5TripApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafy_5_Trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssafy5TripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssafy5TripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/ssafy_5_Trip/Ssafy5TripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafy_5_Trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssafy5TripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
